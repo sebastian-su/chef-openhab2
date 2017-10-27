39789bbf9b87096f38d1b3eb7d57d2efc7f0b7d2
Sebastian Sucker <sebastian@endocode.com>
2017-10-27 18:58:16 +0200

    check for port 8080 to be listening

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -14,3 +14,19 @@ describe service('openhab2') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe port(8080) do
+  before do
+    60.times do
+      if system("netstat -ntl | grep :8080 ")
+        puts "openhab2 service is up"
+        return 0
+      else
+        sleep(1)
+        puts "still waiting for service..."
+        next
+      end
+    end
+  end
+  it{ should be_listening }
+end
