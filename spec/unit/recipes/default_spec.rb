c8853201d341d2322ff404c0a765fbdbf6817155
Sebastian Sucker <sebastian@endocode.com>
2017-10-29 20:41:29 +0100

    adding spec tests & coverage call

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,14 +7,19 @@
 require 'spec_helper'
 
 describe 'openhab2::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  context 'on ubuntu 16.04 with default attributes' do
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'installs & configures openhab2' do
+      expect(chef_run).to install_package('openhab2')
+      expect(chef_run).to install_package('apt-transport-https')
+      expect(chef_run).to add_apt_repository('openhab2')
+      expect(chef_run).to start_service('openhab2')
+    end
   end
+
 end
